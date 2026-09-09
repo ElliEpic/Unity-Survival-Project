44097b4d3dec8af99dcb555ba47063e62922a19b
Ellinor <ellinor.ribbing@icloud.com>
2026-09-09 20:00:09 +0200

    Added enemy, boundaries, shadows, walking animatons

--- Assets/Scripts/Player Movement.cs
+++ Assets/Scripts/Player Movement.cs
@@ -1,27 +1,40 @@
+
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
     [SerializeField] private float movespeed = 2.0f;
     public Vector3 playerMoveDirection;
- 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
     }
 
-    // Update is called once per frame
     void Update()
     {
-
         float inputX = Input.GetAxisRaw("Horizontal");
         float inputY = Input.GetAxisRaw("Vertical");
-        playerMoveDirection = new Vector2(inputX, inputY).normalized;
 
-        rb.linearVelocity = new Vector2(playerMoveDirection.x * movespeed, playerMoveDirection.y * movespeed);
+        playerMoveDirection = new Vector3(inputX, inputY).normalized;
+
+        animator.SetFloat("moveX", inputX);
+        animator.SetFloat("moveY", inputY);
+
+        if (playerMoveDirection == Vector3.zero)
+        {
+            animator.SetBool("moving", false);
+        }
+        else
+        {
+            animator.SetBool("moving", true);
+        }
+
+        rb.linearVelocity = new Vector3(
+            playerMoveDirection.x * movespeed,
+            playerMoveDirection.y * movespeed
+        );
     }
 }
